5a650d80cdddd031cb3434e73483ec891c8dde35
Cameron McEfee <cm@cameronmcefee.com>
2020-07-30 14:11:32 -0700

    show results side by side

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SEO from '../components/seo';
 
 import SentryAlgolia from '../sentry-algolia';
 
-const search = new SentryAlgolia(['blog']);
+const search = new SentryAlgolia(['docs', 'develop', 'help-center', 'blog']);
 
 class IndexPage extends React.Component {
   constructor() {
@@ -37,32 +37,49 @@ class IndexPage extends React.Component {
     return (
       <Layout>
         <SEO title="Home" />
+
+        <p>
+          This searches across all supported Sentry sites. <br />
+          <a href="https://github.com/getsentry/sentry-algolia">
+            View on GitHub
+          </a>
+        </p>
+
         <input
           type="text"
           value={this.state.query}
           onChange={({ target }) => this.setQuery(target.value)}
         />
-        {this.state.results.map(result => {
-          return (
-            <div key={result.site}>
-              <h1>{result.name}</h1>
-              {result.hits.map(hit => (
-                <li key={hit.id}>
-                  <strong>
-                    <a href={hit.url}>
-                      <span
-                        dangerouslySetInnerHTML={{ __html: hit.title }}
-                      ></span>
-                    </a>
-                  </strong>
-                  <br />
+        <div style={{ display: 'flex', margin: '-1rem' }}>
+          {this.state.results.map(result => {
+            return (
+              <div key={result.site} style={{ flex: 1, padding: '1rem' }}>
+                <h2 style={{ fontSize: '1rem' }}>{result.name}</h2>
+                {result.hits.map(hit => (
+                  <li
+                    key={hit.id}
+                    style={{
+                      listStyleType: 'none',
+                      marginBottom: '16px',
+                      fontSize: '.875rem',
+                    }}
+                  >
+                    <strong>
+                      <a href={hit.url}>
+                        <span
+                          dangerouslySetInnerHTML={{ __html: hit.title }}
+                        ></span>
+                      </a>
+                    </strong>
+                    <br />
 
-                  <span dangerouslySetInnerHTML={{ __html: hit.text }} />
-                </li>
-              ))}
-            </div>
-          );
-        })}
+                    <span dangerouslySetInnerHTML={{ __html: hit.text }} />
+                  </li>
+                ))}
+              </div>
+            );
+          })}
+        </div>
       </Layout>
     );
   }
