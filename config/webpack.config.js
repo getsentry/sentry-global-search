e2bce24484d419dae7e08bfac4b728b16cbafbcb
Cameron McEfee <cm@cameronmcefee.com>
2020-08-21 15:07:56 -0700

    Add search component

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -9,6 +9,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
--- src/search-demo.js
+++ src/search-demo.js
@@ -1,91 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { SentryGlobalSearch } from '../';
+import Search from './sentry-global-search/components/search';
 
-const search = new SentryGlobalSearch([
-  'docs',
-  'develop',
-  'help-center',
-  'blog',
-]);
+const styles = require('./sentry-global-search/scss/styles.scss');
 
-class SearchDemo extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      query: 'react',
-      results: [],
-    };
+const SearchDemo = () => (
+  <div style={{ fontFamily: 'Helvetica' }}>
+    <p>
+      This searches across all supported Sentry sites. <br />
+      <a href="https://github.com/getsentry/sentry-global-search">
+        View on GitHub
+      </a>{' '}
+      | <a href="/parse/">View Parser demo</a>
+    </p>
 
-    this.setQuery = this.setQuery.bind(this);
-    this.searchQuery = this.searchQuery.bind(this);
-  }
-
-  componentDidMount() {
-    this.searchQuery();
-  }
-
-  searchQuery() {
-    search.query(this.state.query).then(results => {
-      this.setState({ results });
-    });
-  }
-
-  setQuery(query) {
-    this.setState({ query }, this.searchQuery);
-  }
-
-  render() {
-    return (
-      <>
-        <p>
-          This searches across all supported Sentry sites. <br />
-          <a href="https://github.com/getsentry/sentry-global-search">
-            View on GitHub
-          </a>{' '}
-          | <a href="/parse/">View Parser demo</a>
-        </p>
-
-        <input
-          type="text"
-          value={this.state.query}
-          onChange={({ target }) => this.setQuery(target.value)}
-        />
-        <div style={{ display: 'flex', margin: '1rem -1rem -1rem' }}>
-          {this.state.results.map(result => {
-            return (
-              <div key={result.site} style={{ flex: 1, padding: '1rem' }}>
-                <h2 style={{ fontSize: '1rem' }}>{result.name}</h2>
-                {result.hits.map(hit => (
-                  <li
-                    key={hit.id}
-                    style={{
-                      listStyleType: 'none',
-                      marginBottom: '16px',
-                      fontSize: '.875rem',
-                    }}
-                  >
-                    <strong>
-                      <a href={hit.url}>
-                        <span
-                          dangerouslySetInnerHTML={{ __html: hit.title }}
-                        ></span>
-                      </a>
-                    </strong>
-                    <br />
-
-                    <span dangerouslySetInnerHTML={{ __html: hit.text }} />
-                  </li>
-                ))}
-              </div>
-            );
-          })}
-        </div>
-      </>
-    );
-  }
-}
+    <Search />
+  </div>
+);
 
 const wrapper = document.getElementById('container');
 wrapper ? ReactDOM.render(<SearchDemo />, wrapper) : false;
