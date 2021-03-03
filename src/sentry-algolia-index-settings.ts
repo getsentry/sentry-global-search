c58ed4be28235426cee367a95f4cdf2bd162379a
Cameron McEfee <cm@cameronmcefee.com>
2021-03-03 10:25:03 -0800

    retrieve context

--- src/sentry-algolia-index-settings.ts
+++ src/sentry-algolia-index-settings.ts
@@ -18,7 +18,7 @@ const settings: Settings = {
   ],
   distinct: 3,
   attributeForDistinct: 'section',
-  attributesToRetrieve: ['text', 'title', 'section', 'url', 'anchor'],
+  attributesToRetrieve: ['text', 'title', 'section', 'url', 'anchor', 'context'],
   searchableAttributes: ['unordered(section)', 'unordered(keywords)', 'text'],
   customRanking: ['asc(sectionRank)', 'desc(position)'],
   disableTypoToleranceOnWords: ['react', 'cli'],
